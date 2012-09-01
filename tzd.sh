754e32ef900499d56696a705fd4f6e6b69b3fd82
repo <f2b@gmx.ch>
2012-09-01 23:26:24 +0200

    first commit

---
+++ tzd.sh
@@ -0,0 +1,15 @@
+tzd () {
+  if [ "$1" != "" ]
+  then
+    URL=`echo "$1"|awk -F = '{print "http://recordings-1.20min-tv.ch:/videos/"$3"m.flv"}'`
+    echo "URL: $URL"
+    if [ `which curl 2>/dev/null` ]
+    then
+      curl -O $URL
+    elif [ `which wget 2>/dev/null` ]
+    then
+      wget $URL
+    fi
+  fi
+}
+
