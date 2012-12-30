b88d21e28549dce4b6cef25c789924bef22d6b68
repro <f2b@gmx.ch>
2012-12-30 12:58:52 +0100

    Update tzd.sh
    remote URL updated

--- tzd.sh
+++ tzd.sh
@@ -1,12 +1,10 @@
 tzd () {
   if [ "$1" != "" ]
   then
-    URL=`echo "$1"|awk -F = '{print "http://recordings-1.20min-tv.ch:/videos/"$3"m.flv"}'`
+#   URL=`echo "$1"|awk -F = '{print "http://recordings-1.20min-tv.ch:/videos/"$3"m.flv"}'`
+    URL=`echo "$1"|awk -F = '{print "http://server788.20min-tv.ch:/videos/"$3"m.flv"}'`
     echo "URL: $URL"
-    if [ `which curl 2>/dev/null` ]
-    then
-      curl -O $URL
-    elif [ `which wget 2>/dev/null` ]
+    if [ `which wget 2>/dev/null` ]
     then
       wget -t 1 $URL
     fi
