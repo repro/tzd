c8c1a06dd41d77e2ad187739a8073f2a40940005
repro <f2b@gmx.ch>
2013-01-06 18:24:35 +0100

    Update tzv.sh

--- tzv.sh
+++ tzv.sh
@@ -4,7 +4,7 @@ tzv () {
 #   URL=`echo "$1"|awk -F = '{print "http://recordings-1.20min-tv.ch:/videos/"$3"m.flv"}'`
     URL=`echo "$1"|awk -F = '{print "http://server788.20min-tv.ch:/videos/"$3"m.flv"}'`
     echo "URL: $URL"
-    if [ `which wget 2>/dev/null` ]
+    if [ `which mplayer 2>/dev/null` ]
     then
       mplayer $URL
     fi
