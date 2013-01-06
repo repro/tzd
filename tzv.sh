f60fb76294e84c9467097152d4593db391e2d0b2
repro <f2b@gmx.ch>
2013-01-06 09:23:51 -0800

    Create tzv.sh

---
+++ tzv.sh
@@ -0,0 +1,14 @@
+tzv () {
+  if [ "$1" != "" ]
+  then
+#   URL=`echo "$1"|awk -F = '{print "http://recordings-1.20min-tv.ch:/videos/"$3"m.flv"}'`
+    URL=`echo "$1"|awk -F = '{print "http://server788.20min-tv.ch:/videos/"$3"m.flv"}'`
+    echo "URL: $URL"
+    if [ `which wget 2>/dev/null` ]
+    then
+      mplayer $URL
+    fi
+  fi
+}
+
+tzv $*
